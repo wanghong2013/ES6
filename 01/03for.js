4334395b41bc88e706bd274fe515a1e30f56262b
wanghong <3428413@qq.com>
2017-10-20 17:16:36 +0800

    for循环

---
+++ 01/03for.js
@@ -0,0 +1,26 @@
+//for循环 
+
+
+//es3中用var声明的变量是全局变量。
+
+//for(var i=0;i<3;i++){
+    //to do
+//}
+
+//console.log(i); //3
+
+
+//es5中用let声明的变量有作用域块
+
+//for(let i=0;i<3;i++){
+
+    //to do
+//}
+
+//console.log(i); //报错 i is not defined
+
+
+for(var i=0;i<3;i++){
+    console.log(i);
+}
+
