674d151a451feac0946b4a9e5cc2f824047e5932
黷黷黷 <34284143@qq.com>
2018-07-20 08:23:03 +0800

    模板字符串

---
+++ 08模板字符串/index.js
@@ -0,0 +1,27 @@
+let firstName = 'Flowke',
+    lastName= 'Hurie';
+
+let string ='myName is ' + firstName +' ' + lastName;
+
+console.log(string);
+
+//模板字符串中可以直接使用变量  ${ 变量 }
+
+//声明模板字符串使用反引号 ` `
+let s = `myName is ${firstName} + ${lastName}`
+
+console.log(s);
+
+
+function fn(){
+
+    return 'mike'
+}
+
+//模板字符串中还可以写表达式 
+let str2 = ` my name is ${fn()}`
+console.log( str2 );
+
+
+let str3 = `my name is ${3>2?'moli':'xiaoming'}`;
+console.log(str3);
